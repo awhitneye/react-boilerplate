008215a4856ea6cd1772e686164b3641ee8a3204
Andrea <ellisona@gmail.com>
2018-05-11 14:24:41 -0400

    update to sytling of components

--- app/containers/HomePage/Button.js
+++ app/containers/HomePage/Button.js
@@ -5,9 +5,14 @@ const Button = styled.button`
   border-radius: 4px;
   -webkit-font-smoothing: antialiased;
   -webkit-touch-callout: none;
+  user-select: none;
+  cursor: pointer;
+  outline: 0;
+  font-family: 'Helvetica Neue', Helvetica, Arial, sans-serif;
   font-weight: bold;
-  border: 2px solid #2ead92;
-  color: #2ead92;
+  font-size: 16px;
+  border: 2px solid #41addd;
+  color: #41addd;
 `;
 
 export default Button;
